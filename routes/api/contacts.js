3ccb390363a5e0daccf6419721a7df61085ac0a2
ihorkoi <koleev.igor@gmail.com>
2024-02-05 18:46:27 +0200

    add contacts router

---
+++ routes/api/contacts.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import ctrl from "../../controllers/contacts.js";
+import { authenticate } from '../../middlewares/authenticate.js';
+// import { registerSchema, loginSchema } from '../../models/Users.js';
+import { validateBody } from '../../middlewares/validateBody.js';
+
+
+
+export const contactsRouter = express.Router();
+
+contactsRouter.get("/", ctrl.getContacts)
+
+contactsRouter.post('/add', validateBody(), ctrl.addContact)
+
+contactsRouter.put('/:id', validateBody(), ctrl.updateContact)
+
+contactsRouter.delete("/:id", ctrl.deleteContact)
+
+
